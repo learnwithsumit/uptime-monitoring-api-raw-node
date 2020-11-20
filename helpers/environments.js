d73effb9e8b00a704b9ddba4a1a7048024a3f65d
learnwithsumit <play@learnwithsumit.com>
2020-11-20 17:50:52 +0600

    lesson-2 source code first time commit

---
+++ helpers/environments.js
@@ -0,0 +1,37 @@
+/*
+ * Title: Environments
+ * Description: Handle all environment related things
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 11/20/2020
+ *
+ */
+
+// dependencies
+
+// module scaffolding
+const environments = {};
+
+// staging environment
+environments.staging = {
+    port: 3000,
+    envName: 'staging',
+};
+
+// production environment
+environments.production = {
+    port: 5000,
+    envName: 'production',
+};
+
+// determine which environment was passed
+const currentEnvironment =
+    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+
+// export corresponding environment object
+const environmentToExport =
+    typeof environments[currentEnvironment] === 'object'
+        ? environments[currentEnvironment]
+        : environments.staging;
+
+// export module
+module.exports = environmentToExport;
